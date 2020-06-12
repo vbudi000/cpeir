f02893f65e0d04f67e4b96fa63205033be17e7a2
Budi Darmawan <vbudi@us.ibm.com>
2020-06-11 21:18:48 -0500

    v1.04

--- runtime/install/cp4multicloud-1.3.0-icam.sh
+++ runtime/install/cp4multicloud-1.3.0-icam.sh
@@ -8,24 +8,30 @@ oc create secret docker-registry icr-io \
     --docker-username=cp \
     --docker-server="cp.icr.io" \
     --docker-password=${entitlement} \
-    --docker-email="vbudi@us.ibm.com"
+    --docker-email="vbudi@us.ibm.com" \
+    -n kube-system
+
+oc patch serviceaccount default -p '{"imagePullSecrets": [{"name": "icr-io"}]}' -n kube-system
+
+oc get secret tiller-secret -n kube-system -o yaml | grep -A5 '^data:' |awk -F: '{system("echo "$2" | base64 -d >"$1)}'
+helm init --client-only
+cp ca.crt ~/.helm/ca.pem
+cp tls.key ~/.helm/key.pem
+cp tls.crt ~/.helm/cert.pem
 
 # collect cluster info
-sc=$(oc get sc --no-headers -o custom-columns=name:metadata.name | head -n 1)
-workers=$(oc get node --no-headers -o  custom-columns=name:metadata.name -l node-role.kubernetes.io/worker | paste -s -d, -)
-roks="false"
-roks_url='""'
+icpconsole=$(oc get configmap ibmcloud-cluster-info -n kube-public -o=jsonpath='{.data.cluster_address}')
+icpproxy=$(oc get configmap ibmcloud-cluster-info -n kube-public -o=jsonpath='{.data.proxy_address}')
 
 # Create configuration files
-cat cp4multicloud-1.3.0-configMap.yaml | \
-    sed "s/<entitlement>/${entitlement}/g" | \
-    sed "s/<workers>/${workers}/g" | \
-    sed "s/<sc>/${sc}/g" | \
-    sed "s/<roks>/${roks}/g" | \
-    sed "s/<roks_url>/${roks_url}/g"| oc create -f -
+helm repo add entitled https://raw.githubusercontent.com/IBM/charts/master/repo/entitled/index.yaml
 
-# Create installation Job
-oc apply -f cp4multicloud-1.3.0-install.yaml
+echo helm install --tls ibm-cloud-appmgmt-prod -n ibmcloudappmgmt --namespace kube-system \
+      --set global.ingress.domain=${icpconsole}, \
+            global.ingress.port=443, \
+            global.icammcm.domain=${icpproxy}, \
+            global.masterIP=${icpconsole}, \
+            global.masterPort=443
 
 # loop to check installation
 # Install features
