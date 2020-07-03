adbd282da1ff23533f434f283e6a566aea8cdbd0
Budi Darmawan <vbudi@us.ibm.com>
2020-07-03 15:37:12 -0500

    v1.08

--- files/install/cp4multicloud-1.3.0.sh
+++ files/install/cp4multicloud-1.3.0.sh
@@ -6,6 +6,7 @@ name="${fn%.*}"
 entitlement=$1
 # Installing
 if [ -z $entitlement ]; then
+  echo {}
   exit 9
 fi
 
@@ -35,15 +36,140 @@ else
 fi
 cd /install
 # Create configuration files
-cat cp4multicloud-1.3.0-configMap.yaml | \
+cat <<EOF | \
     sed "s/<entitlement>/${entitlement}/g" | \
     sed "s/<workers>/${workers}/g" | \
     sed "s/<sc>/${sc}/g" | \
     sed "s/<roks>/${roks}/g" | \
     sed "s/<roks_url>/${roks_url}/g"| oc create -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+name: cp4multicloud-1.3.0-configmap
+data:
+config.yaml: |
+  cluster_nodes:
+    master: [<workers>]
+    proxy: [<workers>]
+    management: [<workers>]
+  # Storage Class
+  storage_class: <sc>
+  roks_enabled: <roks>
+  roks_url: <roks_url>
+  roks_user_prefix: "IAM#"
+  image_repo: cp.icr.io/cp/icp-foundation
+  private_registry_enabled: true
+  docker_username: cp
+  docker_password: <entitlement>
+  password_rules:
+  - '(.*)'
+  default_admin_password: passw0rd
+  management_services:
+    # Common services
+    iam-policy-controller: enabled
+    metering: enabled
+    licensing: disabled
+    monitoring: enabled
+    nginx-ingress: enabled
+    common-web-ui: enabled
+    catalog-ui: enabled
+    mcm-kui: enabled
+    logging: disabled
+    audit-logging: disabled
+    system-healthcheck-service: disabled
+    multitenancy-enforcement: disabled
+
+    # mcm services
+    multicluster-hub: enabled
+    search: enabled
+    key-management: enabled
+    notary: disabled
+    cis-controller: disabled
+    vulnerability-advisor: disabled
+    mutation-advisor: disabled
+    sts: disabled
+    secret-encryption-policy-controller: disabled
+    image-security-enforcement: disabled
+hosts: |
+  [master]
+  localhost ansible_connection=local
+
+  [worker]
+  localhost ansible_connection=local
+
+  [proxy]
+  localhost ansible_connection=local
+
+  #[management]
+  #localhost ansible_connection=local
+
+  #[va]
+  #localhost ansible_connection=local
+EOF
+
 
 # Create installation Job
-oc apply -f cp4multicloud-1.3.0-install.yaml
+cat <<EOF | oc create -f -
+kind: Job
+apiVersion: batch/v1
+metadata:
+  name: cp4multicloud-1.3.0-installer
+spec:
+  parallelism: 1
+  completions: 1
+  backoffLimit: 0
+  template:
+    spec:
+      restartPolicy: Never
+      serviceAccountName: cpeir
+      imagePullSecrets:
+        - name: icr-io
+      securityContext: {}
+      initContainers:
+        - name: build-config
+          image: busybox:1.28
+          volumeMounts:
+            - name: config
+              mountPath: /config
+            - name: file
+              mountPath: /file
+          command:
+            - /bin/sh
+            - '-c'
+            - >-
+              mkdir -p /config/misc/storage_class &&
+              cp /file/config.yaml /config/config.yaml &&
+              cp /file/hosts /config/hosts &&
+              echo "done"
+      containers:
+        - resources: {}
+          terminationMessagePath: /dev/termination-log
+          name: installer
+          args:
+            - install-with-openshift
+          env:
+            - name: LICENSE
+              value: accept
+            - name: ANSIBLE_LOCAL_TEMP
+              value: /tmp
+          imagePullPolicy: Always
+          volumeMounts:
+            - name: config
+              mountPath: /installer/cluster
+            - name: cloudctl
+              mountPath: /.cloudctl
+          terminationMessagePolicy: File
+          image: cp.icr.io/cp/icp-foundation/mcm-inception:3.2.5
+      serviceAccount: cpeir
+      volumes:
+        - name: cloudctl
+          emptyDir: {}
+        - name: config
+          emptyDir: {}
+        - name: file
+          configMap:
+            name: cp4multicloud-1.3.0-configmap
+EOF
 
 # loop to check installation
 # Install features
