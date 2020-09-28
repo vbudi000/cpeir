941f4ad971a7b0d14653e3f526667bb76a8a05ba
Budi Darmawan <vbudi@us.ibm.com>
2020-09-28 10:00:16 -0500

    New expanded

--- files/check/cp4multicloud-1.2.0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-filename=$(basename $0)
-name="${filename%.*}"
-version=$(echo ${name} | cut -d- -f2)
-echo "{\"installed\": true, \"name\": \"${name}\", \"version\": \"${version}\"}"
-exit 0
